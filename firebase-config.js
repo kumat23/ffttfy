f7b77e1b1a838c1e1958b3549f258b1b2b42b27f
kumat23 <155309178+kumat23@users.noreply.github.com>
2023-12-31 19:29:29 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7WvFBqsNWfuWuVjJGd3UkUgIkzxvHB_Y",
+  authDomain: "vudr-e500d.firebaseapp.com",
+  databaseURL: "https://vudr-e500d-default-rtdb.firebaseio.com",
+  projectId: "vudr-e500d",
+  storageBucket: "vudr-e500d.appspot.com",
+  messagingSenderId: "1024002486473",
+  appId: "1:1024002486473:web:80b997b5fa05ae7a2add18"
+};
